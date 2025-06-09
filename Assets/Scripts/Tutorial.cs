4bd5e2218487258762ebeafe63373e4f96c8d4ea
Aladdin6san <110479453+Aladdin6san@users.noreply.github.com>
2025-06-08 22:13:43 -0300

    Ajustes e teste

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class TutorialControlador : MonoBehaviour
 {
     public GameObject tutorial;
+    private static bool tutorialJaExibido = false;
     void Start()
     {
-        StartCoroutine(Painel());
+        if (!tutorialJaExibido)
+        {
+            tutorial.SetActive(true);
+            StartCoroutine(Painel());
+            tutorialJaExibido = true; // marca como já exibido
+        }
+        else
+        {
+            tutorial.SetActive(false);
+        }
     }
 
     void Update()
@@ -19,9 +30,13 @@ public class TutorialControlador : MonoBehaviour
         yield return new WaitForSeconds(3f);
         tutorial.SetActive(false);
     }
-
     public void QuitToMenu()
     {
+        if (Item.ObterContador() < 6)
+        {
+            Item.DefinirContador(0);
+        }
+
         tutorial.SetActive(false);
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
